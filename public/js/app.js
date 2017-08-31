5e8f4d2798944f90ca9f1c90637a1523c14bd158
adominguez <dmartin.alberto@gmail.com>
2017-08-31 22:33:42 +0200

    add sent messages

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,11 @@ socket.on('connect', function() {
 socket.on('message', function(message) {
     console.log('new message');
     console.log(message.text)
+
+    var messages = document.querySelector('#messages');
+
+    setMessage(message.text);
+
 });
 
 // Handles summitting of new message
@@ -18,4 +23,14 @@ form.addEventListener('submit', function(event) {
     socket.emit('message', {
         text: input.value
     });
+    setMessage(input.value);
 });
+
+function setMessage(message) {
+    var messages = document.querySelector('#messages');
+
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode(message);
+    node.appendChild(textnode);
+    messages.appendChild(node);
+}
